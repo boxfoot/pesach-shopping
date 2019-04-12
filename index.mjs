4510c9d618ecfd8dad97191b17224852f94845b7
Benj Kamm <benj.kamm@gmail.com>
2019-04-11 22:10:00 -0500

    Convert db to JSON

--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@ import generateShoppingList from "./lib/shopping-list";
 // DB imports
 import recipes from "./db/recipes";
 import groceries from "./db/groceries";
-import menu from "./db/menu";
+import menu from "./db/menu-5778";
 
 const myList = generateShoppingList(menu, recipes, groceries);
 
